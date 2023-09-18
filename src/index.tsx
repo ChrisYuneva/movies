b613e2e0f5748ca2171e7e70c1acf8ee1c079088
ChrisYuneva <christina_yuneva@mail.ru>
2023-09-18 13:40:06 +0400

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,25 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>
 );
